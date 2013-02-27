edc9fc135c35b41082e792a9392bc4d92a375031
Pascal Hartig <phartig@rdrei.net>
2013-02-27 23:51:18 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('soccerStatsFrontendApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
