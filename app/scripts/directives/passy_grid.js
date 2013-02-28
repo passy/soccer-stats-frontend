219e9cc2fcbae4f421937b1fdebea1fa5334992b
Pascal Hartig <phartig@rdrei.net>
2013-02-28 18:32:44 +0100

    More grid fun

--- app/scripts/directives/passy_grid.js
+++ app/scripts/directives/passy_grid.js
@@ -6,7 +6,21 @@
       return {
         restrict: 'A',
         templateUrl: 'views/_passy_grid.html',
+        scope: true,
         link: function postLink(scope, element, attrs) {
+          var options = scope.$eval(attrs.passyGrid);
+
+          scope.headers = options.columnDefs.map(function (x) {
+            return x.displayName;
+          });
+
+          scope.columns = options.columnDefs.map(function (x) {
+            return x.field;
+          });
+
+          scope.$watch(options.data, function (data) {
+            scope.data = data;
+          });
         }
       };
     });
