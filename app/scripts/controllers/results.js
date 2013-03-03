fccfcc0d4c5a08320026f93c82e235cc5f692083
Pascal Hartig <phartig@rdrei.net>
2013-03-03 11:57:36 +0100

    Removed combined score

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -5,13 +5,6 @@
     .controller('ResultsCtrl', function ($scope, appStorage) {
       $scope.results = appStorage.getResults();
 
-      // Very expensive and unnecessary. Should be lazily generated.
-      $scope.$watch('results', function (value) {
-        value.map(function (item) {
-          item.goalsFormatted = String(item.goalsHome) + ':' + item.goalsAway;
-        });
-      }, true);
-
       $scope.addRow = function () {
         $scope.results.push({
           teamHome: 'Team A',
@@ -38,8 +31,11 @@
             options: appStorage.getTeams()
           }
         }, {
-          displayName: 'Score',
-          field: 'goalsFormatted'
+          displayName: 'Home Score',
+          field: 'goalsHome'
+        }, {
+          displayName: 'Away Score',
+          field: 'goalsAway'
         }]
       };
 
