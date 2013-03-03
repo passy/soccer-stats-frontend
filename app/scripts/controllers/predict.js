5de114e8b1d347303f22eaba6399e62bbc8026fc
Pascal Hartig <phartig@rdrei.net>
2013-03-03 22:38:17 +0100

    Finished predict screen

--- app/scripts/controllers/predict.js
+++ app/scripts/controllers/predict.js
@@ -4,5 +4,21 @@
   angular.module('soccerApp')
     .controller('PredictCtrl', function ($scope, appStorage) {
       $scope.scores = appStorage.getScores();
+
+      var teamChanged = function () {
+        var scoreA = $scope.scores[$scope.teamA],
+          scoreB = $scope.scores[$scope.teamB];
+
+        if (scoreA && scoreB) {
+          $scope.result = scoreA - scoreB;
+          $scope.winner = $scope.result >= 0 ? $scope.teamA : $scope.teamB;
+          $scope.resultDiff = Math.round(Math.abs($scope.result));
+        } else {
+          $scope.result = null;
+        }
+      };
+
+      $scope.$watch('teamA', teamChanged);
+      $scope.$watch('teamB', teamChanged);
     });
 }());
